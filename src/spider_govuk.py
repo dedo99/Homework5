e2c959d811a8222fc7e80ad11bbacd0b1061b325
dedo99 <72614855+dedo99@users.noreply.github.com>
2023-01-23 00:27:46 +0100

    Scraping di più dati per GovUK

--- src/spider_govuk.py
+++ src/spider_govuk.py
@@ -5,6 +5,7 @@ from scrapy.item import Item, Field
 
 prefix_url = 'https://find-and-update.company-information.service.gov.uk'
 
+
 class CustomItem(Item):
     name = Field()
     company_number = Field()
@@ -24,7 +25,10 @@ class MySpider(scrapy.Spider):
         yield scrapy.Request('https://find-and-update.company-information.service.gov.uk/search/companies?q=software', self.parse)
         yield scrapy.Request('https://find-and-update.company-information.service.gov.uk/search/companies?q=food', self.parse)
         yield scrapy.Request('https://find-and-update.company-information.service.gov.uk/search/companies?q=all', self.parse)
-
+        # caratteri = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "z"]
+        # for i in caratteri:
+        #     yield scrapy.Request('https://find-and-update.company-information.service.gov.uk/search/companies?q=' + i, self.parse)
+        # yield scrapy.Request('https://find-and-update.company-information.service.gov.uk/search/companies?q=p', self.parse)
 
     def parse(self, response):
         for href in response.xpath('//*[@id="results"]/li/h3/a/@href').getall():
@@ -35,25 +39,23 @@ class MySpider(scrapy.Spider):
         for next_page in response.css('#next-page'):
             yield response.follow(next_page, self.parse)
 
-
     def information_companies(self, response):
         start_time = time.time()
         item = CustomItem()
         item['name'] = self.split_and_replace(response.xpath('//*[@id="content-container"]/div[1]/p[1]/text()').get())
         item['company_number'] = self.split_and_replace(response.xpath('//*[@id="company-number"]/strong/text()').get())
-        item['registered_office_address'] = self.split_and_replace(response.xpath('//div[@class="govuk-tabs__panel"]/dl/dd/text()').get())
+        item['registered_office_address'] = self.split_and_replace(
+            response.xpath('//div[@class="govuk-tabs__panel"]/dl/dd/text()').get())
         item['company_status'] = self.split_and_replace(response.xpath('//*[@id="company-status"]/text()').get())
         item['company_type'] = self.split_and_replace(response.xpath('//*[@id="company-type"]/text()').get())
-        item['company_creation_date'] = self.split_and_replace(response.xpath('//*[@id="company-creation-date"]/text()').get())
+        item['company_creation_date'] = self.split_and_replace(
+            response.xpath('//*[@id="company-creation-date"]/text()').get())
         item['nature_of_business'] = self.split_and_replace(response.xpath('//*[@id="sic0"]/text()').get())
         end_time = time.time()
         item['time'] = end_time - start_time
         return item
 
-
     def split_and_replace(self, text):
         text = text.strip()
         text = text.replace("\n", "")
         return text
-
-
